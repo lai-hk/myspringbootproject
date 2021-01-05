5e106f1091286fdbd4ea7ee0b9a159c70fd27611
lai-hk <863086902@qq.com>
2021-01-05 14:29:27 +0800

    ceshi7

--- src/main/java/com/springboot/ci/myspringbootproject/controller/HelloController.java
+++ src/main/java/com/springboot/ci/myspringbootproject/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.springboot.ci.myspringbootproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
 
     @ResponseBody
-    @RequestMapping("/helloworld")
+    @PostMapping("/helloworld")
     public  String helloworld(){
         System.out.println("helloworld");
         return  "helloworld1122";
